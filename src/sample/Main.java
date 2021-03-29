44ec232ff0724a14ef77d0b72e77968612c8c362
timofeya123 <timofeya123@inbox.ru>
2021-03-29 21:25:54 +0600

    event CSS

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,11 +7,13 @@ import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
@@ -20,6 +22,7 @@ public class Main extends Application {
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml")); // мало ли, вдруг понадобиться ¯\_(ツ)_/¯
 
         // -------------------------------------------------------------- создание основных объектов
+        Label label = new Label("Добро пожаловать");
         Text text1 = new Text("Email"); // создание метки(обычного текста)
         Text text2 = new Text("Password");
 
@@ -42,14 +45,16 @@ public class Main extends Application {
         gridPane.setAlignment(Pos.CENTER); //Рсположение объектов, по центру
 
         //-------------------------------------------------------- добавление всех объектов на панель
-        gridPane.add(text1, 0, 0);
-        gridPane.add(textField1, 1, 0);
-        gridPane.add(text2, 0, 1);
-        gridPane.add(textField2, 1, 1);
-        gridPane.add(button1, 0, 2);
-        gridPane.add(button2, 1, 2);
+        gridPane.add(label, 1, 0);
+        gridPane.add(text1, 0, 1);
+        gridPane.add(textField1, 1, 1);
+        gridPane.add(text2, 0, 2);
+        gridPane.add(textField2, 1, 2);
+        gridPane.add(button1, 0, 3);
+        gridPane.add(button2, 1, 3);
 
         // ------------------------------------------------------------------------ Стили CSS
+        // 1 способ прописывать стили
         button1.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;"); // заливка (darkslateblue), цвет текста (white)
         button2.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;");
 
@@ -57,9 +62,43 @@ public class Main extends Application {
         text2.setStyle("-fx-font: normal bold 20px 'serif' ");
         gridPane.setStyle("-fx-background-color: BEIGE;");// заливка панели
 
+        // действия при наведении мышки (hover)
+        button1.setOnMouseEntered(event -> { // наведение
+            button1.setStyle("-fx-background-color: gold; -fx-text-fill: black;");
+        });
+        button1.setOnMouseExited(event -> { // увести мышку
+            button1.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;");
+        });
+        button1.setOnMousePressed(event -> { // нажать
+            button1.setStyle("-fx-background-color: black; -fx-text-fill: white;");
+        });
+        button1.setOnMouseClicked(event -> { // сделать клик мышки
+            button1.setStyle("-fx-background-color: gold; -fx-text-fill: white;");
+        });
+
+        //всё тоже самое, но для 2 кнопки
+        button2.setOnMouseEntered(event -> {// наведение
+            button2.setStyle("-fx-background-color: gold; -fx-text-fill: black;");
+        });
+        button2.setOnMouseExited(event -> {// увести мышку
+            button2.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;");
+        });
+        button2.setOnMousePressed(event -> {// нажать
+            button2.setStyle("-fx-background-color: black; -fx-text-fill: white;");
+        });
+        button2.setOnMouseClicked(event -> {// сделать клик мышки
+            button2.setStyle("-fx-background-color: gold; -fx-text-fill: white;");
+        });
+
         //-------------------------------------------------------------- настройка формы
         primaryStage.setTitle("CSS Пример"); // заголовок формы(окна)
-        primaryStage.setScene(new Scene(gridPane));//добавить панель на форму
+        Scene scene = new Scene(gridPane); // создание сцены
+        //подключение CSS файл
+        scene.getStylesheets().add(getClass().getResource("myCss.css").toExternalForm()); // 2 способ прописывать стили
+        textField1.getStyleClass().add("myStyleTextField"); // добавление стилий через метки
+        textField2.getStyleClass().add("myStyleTextField");
+        // setUserAgentStylesheet - удаление старых стилей
+        primaryStage.setScene(scene);//добавить панель на форму
         primaryStage.show(); // отобразить
     }
 
